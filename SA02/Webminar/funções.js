ef4eadb99b1c7afa1b8d9e8cf9f1eace04cdd0f5
Alexandre Hoss <alexandre.hoss@yahoo.com.br>
2022-08-18 10:40:01 -0300

    Webminar 02

---
+++ SA02/Webminar/funções.js
@@ -0,0 +1,14 @@
+// function saudarComHello(nome, sobrenome) {
+//     console.log(`Boa noite ${nome} ${sobrenome}`);
+    
+// }
+
+// saudarComHello('Alexandre', 'Hoss');
+// saudarComHello('Eduardo', 'Costa');
+
+function somar(x,y) {
+    return x+y
+}
+
+let resultado = somar(5,2)
+console.log(resultado);
