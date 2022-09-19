d8c90b50acf5b4b31e5d9f2393d152bfcaa02971
Alexandre Hoss <alexandre.hoss@yahoo.com.br>
2022-09-19 15:11:02 -0300

    Projeto SA03

--- SA03/ProjetoSA03/README.md
+++ SA03/ProjetoSA03/README.md
@@ -1,4 +1,4 @@
-# ProjetoSA3
+# ProjetoSA03
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.2.3.
 
--- SA03/ProjetoSA03/src/app/app.module.ts
+++ SA03/ProjetoSA03/src/app/app.module.ts
@@ -4,19 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
-import { LoginComponent } from './views/login/login.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
-import { ProdutoComponent } from './views/produto/produto.component';
+import { LoginComponent } from './views/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent,
     FooterComponent,
     HeaderComponent,
-    ProdutoComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
